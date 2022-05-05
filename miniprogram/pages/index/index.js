f9372dabb382ddf8508489d60274b33121757f29
unknown <unknown@example.com>
2022-05-05 18:37:23 +0800

    心态崩了

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-      text:[],
+      text:["../../icon/5c2f114de6d323908267e5f1d4887073.jpeg","../../icon/17a639263ae6ead0d92f36ef7b2ccb13.jpeg","../../icon/169ebd7d9759ad58b68aa02b2954b68a.jpeg"],
    
   },
   // 事件处理函数
@@ -26,6 +26,15 @@ Page({
       })
       .catch(res =>{
         console.log('请求失败',res)
+                  /* 获取系统信息 */
+                  wx.getSystemInfo({
+                    success: function (res) {
+                      that.setData({
+                        winWidth: res.windowWidth,
+                        winHeight: res.windowHeight
+                      });
+                    }
+                  })
     });
 }
 
